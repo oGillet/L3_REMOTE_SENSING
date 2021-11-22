30b3bc2281c4433435fd3460d6ed263a8fbf56f5
olivier.gillet <gillet.olivier@outlook.fr>
2021-11-22 16:57:34 +0100

    test

--- script/clip_landsat8_raster.R
+++ script/clip_landsat8_raster.R
@@ -1,12 +1,12 @@
 start.time <- Sys.time()
 for (d in c('01_JANVIER','04_AVRIL','08_AOUT','10_OCTOBRE')){
-  setwd(paste0("/home/gilleol2/Bureau/L8_BREST/",d,"_2017_L8_BREST"))
+  setwd(paste0("/home/gilleol2/Bureau/L3_TELEDETECTION/L8_BREST/",d,"_2017_L8_BREST"))
   filesList <- list.files(pattern = ".TIF")
-  ze = sf::st_read("/home/gilleol2/Bureau/L8_BREST/ze.shp")
+  ze = sf::st_read("/home/gilleol2/Bureau/L3_TELEDETECTION/L8_BREST/ze.shp")
   for(r in filesList){
     raster_best = raster::raster(r)
     raster_best_crop = raster::crop(raster_best,ze)
-    raster::writeRaster(raster_best_crop,paste0(tools::file_path_sans_ext(r),'_clip.TIF'),options=c('TFW=YES'))
+    raster::writeRaster(raster_best_crop,paste0(tools::file_path_sans_ext(r),'_clip.TIF'),options=c('TFW=YES'),overwrite=TRUE)
   }
 }
 end.time <- Sys.time()
